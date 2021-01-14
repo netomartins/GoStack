90e816e42bc362b7ecdba875a917f4e483055cd6
Neto Martins <netomartins2@hotmail.com>
2021-01-14 00:49:17 -0300

    Configurando os estados da aplicação e começando com o CSS

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from './components/Header';
+import './components/App.css';
 
-function App(){
-  return <h1>Hello GoStack</h1>;
+function App() {
+ 
+  const [projects, setProjects] = useState( ['Desenvolvimento de app', 'Front-end web']);
+ //useState retorna um array com 2 posições
+ //
+ //1. Variavel com o seu valor inicial
+ //2. Função para atualizar esse valor
+
+
+  function handleAddProject() {
+    //projects.push(`Novo projeto ${Date.now()}`);
+    setProjects([ ...projects,`Novo projeto ${Date.now()}` ]);
+
+  }
+
+
+  return (
+    <>
+      <Header title="Projects" />
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
+    </>
+  );
 }
 
 export default App;
