1cd4af73dd4cff11b22972b9a51de48d2efa066c
Neto Martins <netomartins2@hotmail.com>
2021-01-19 12:13:40 -0300

    Conectando o backend ao frontend ok

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,26 +1,36 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import Header from './components/Header';
 import './components/App.css';
 
-function App() {
- 
-  const [projects, setProjects] = useState( ['Desenvolvimento de app', 'Front-end web']);
- //useState retorna um array com 2 posições
- //
- //1. Variavel com o seu valor inicial
- //2. Função para atualizar esse valor
 
+
+function App() {
+  const [projects, setProjects] = useState([
+    'Desenvolvimento de app',
+    'Front-end web',
+  ]);
+
+  useEffect(()=> {
+    api.get('projects').then(response => {
+      console.log(response);
+    })
+  }, []);
+
+  //useState retorna um array com 2 posições
+  //
+  //1. Variavel com o seu valor inicial
+  //2. Função para atualizar esse valor
 
   function handleAddProject() {
     //projects.push(`Novo projeto ${Date.now()}`);
-    setProjects([ ...projects,`Novo projeto ${Date.now()}` ]);
-
+    setProjects([...projects, `Novo projeto ${Date.now()}`]);
   }
-
 
   return (
     <>
       <Header title="Projects" />
+
       <ul>
         {projects.map((project) => (
           <li key={project}>{project}</li>
--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -4,33 +4,33 @@ module.exports = {
   entry: path.resolve(__dirname, './src', './index.js'),
   output: {
     path: path.resolve(__dirname, './public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
 
   devServer: {
-    contentBase:  path.resolve(__dirname, './public'),
+    contentBase: path.resolve(__dirname, './public'),
   },
 
   module: {
     rules: [
       {
-        test:/\.js$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        }
+        },
       },
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: [
-          {loader: 'style-loader'},
-          {loader: 'css-loader'},
-        ]
-
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader',
+        }
       }
-
     ]
   },
-
 };
