5dabc0cc6df12600542cb0202aba9325b63fa256
Neto Martins <netomartins2@hotmail.com>
2021-01-19 18:10:26 -0300

    instalando plugin do @babel, configurando a function handleAddProject com async/await e fazendo o botao adicionar novo projeto funcionar

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,14 +6,11 @@ import './components/App.css';
 
 
 function App() {
-  const [projects, setProjects] = useState([
-    'Desenvolvimento de app',
-    'Front-end web',
-  ]);
+  const [projects, setProjects] = useState([]);
 
   useEffect(()=> {
     api.get('projects').then(response => {
-      console.log(response);
+      setProjects(response.data);
     })
   }, []);
 
@@ -22,9 +19,18 @@ function App() {
   //1. Variavel com o seu valor inicial
   //2. Função para atualizar esse valor
 
-  function handleAddProject() {
+ async function handleAddProject() {
     //projects.push(`Novo projeto ${Date.now()}`);
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+   const response = await api.post('projects', {
+      
+        title: `NodeJs ${Date.now()}`,
+        owner: "Neto Martins"
+      
+    });
+
+    const project =  response.data;
+    setProjects([...projects, project]);
   }
 
   return (
@@ -33,7 +39,7 @@ function App() {
 
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>{project.title}</li>
         ))}
       </ul>
 
