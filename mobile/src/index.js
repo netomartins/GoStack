59b6c50a1de79daa7cfe863a75cc925950608de5
Neto Martins <netomartins2@hotmail.com>
2021-01-20 12:30:36 -0300

    Conectando o mobile com o backend dos projetos (ok)

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import api from './services/api';
 
-// Não possuem valor semantico (significado)
+//Não possuem valor semantico (significado)
 //Não possuem estilização próipria
 //Todos componentes possuem por padrão "Display: flex"
 
@@ -9,13 +10,26 @@ import { View, Text, StyleSheet, StatusBar } from 'react-native';
 //Text: p, span, strong, h1, h2, h3
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
   return (
     <>
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello Neto Martins</Text>
-    </View>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
 
+      <View style={styles.container}>
+        {projects.map((project) => (
+          <Text style={styles.project} key={project.id}>
+            {project.title}
+          </Text>
+        ))}
+      </View>
     </>
   );
 }
@@ -28,9 +42,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  title: {
+  project: {
     color: '#FFF',
-    fontSize: 40,
-    fontWeight: 'bold',
+    fontSize: 20,
   },
 });
---
+++ mobile/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api = axios.create({
+baseURL: 'http://localhost:3333',
+
+});
+
+export default api;
+
+/*
+*ios com emulador: localhost
+*ios com fisico: IP da maquina
+*Android com Emulador: localhost (adb reverse)
+*Android com emulador: 10.0.2.2 (Android studio)
+*Android com emulador: 10.0.3.2 (Genymotion)
+*Android com fisico: IP da maquina
+*/
